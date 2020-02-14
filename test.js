afc09075b943953c0ced24836a4a5d190e323184
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2020-02-14 14:14:59 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
+var facts = {"Sun is big", "Moon is white", "Clocks are round"};
+
+function main() {
+  var elem = document.getElementById("text");
+  elem.innerHTML = facts[Math.floor(Math.random()*facts.length)];;
+}
+
 window.onload = function(){
-  elem = document.findElementById("text");
-  elem.text = "yo";
+  main();
 };
